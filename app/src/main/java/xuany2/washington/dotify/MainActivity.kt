aad212248e7479efc7355f35d6915a51f74e955a
xuany2 <xuany2@uw.edu>
2020-04-03 16:06:13 -0700

    Initial commit

---
+++ app/src/main/java/xuany2/washington/dotify/MainActivity.kt
@@ -0,0 +1,12 @@
+package xuany2.washington.dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
